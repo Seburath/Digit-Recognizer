77f266aaa48ea6a97257599ca15b3d21f05110bc
seburath <seburath@gmail.com>
2019-05-14 18:05:04 -0500

    actualizated

--- digit-recognizer.py
+++ digit-recognizer.py
@@ -19,7 +19,7 @@ from keras.preprocessing.image import ImageDataGenerator
 # For example, running this (by clicking run or pressing Shift+Enter) will list the files in the input directory
 
 from subprocess import check_output
-print(check_output(["ls", "../input"]).decode("utf8"))
+# print(check_output(["ls", "../input"]).decode("utf8"))
 
 # Any results you write to the current directory are saved as output.
 
