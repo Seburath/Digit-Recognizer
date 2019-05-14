cc8ff2e588c513b21f877fc3ec0222cf794d5ce8
seburath <seburath@gmail.com>
2019-05-14 17:44:44 -0500

    train script added

---
+++ digit-recognizer.py
@@ -0,0 +1,33 @@
+# This Python 3 environment comes with many helpful analytics libraries installed
+# It is defined by the kaggle/python docker image: https://github.com/kaggle/docker-python
+# For example, here's several helpful packages to load in 
+
+import numpy as np # linear algebra
+import pandas as pd # data processing, CSV file I/O (e.g. pd.read_csv)
+
+import matplotlib.pyplot as plt
+# %matplotlib inline
+
+from keras.models import Sequential
+from keras.layers import Dense , Dropout , Lambda, Flatten
+from keras.optimizers import Adam ,RMSprop
+from sklearn.model_selection import train_test_split
+from keras import  backend as K
+from keras.preprocessing.image import ImageDataGenerator
+
+# Input data files are available in the "../input/" directory.
+# For example, running this (by clicking run or pressing Shift+Enter) will list the files in the input directory
+
+from subprocess import check_output
+print(check_output(["ls", "../input"]).decode("utf8"))
+
+# Any results you write to the current directory are saved as output.
+
+train = pd.read_csv("train.csv")
+print(train.shape)
+train.head()
+
+test= pd.read_csv("../input/test.csv")
+print(test.shape)
+test.head()
+
